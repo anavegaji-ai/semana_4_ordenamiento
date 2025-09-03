62045fe7f4680451a9ff77082274189426ecca1b
Ana Sofia Vega <ana_vegaji@fet.edu.co>
2025-09-03 09:31:01 -0500

    commit incial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,55 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(10);
+        nums.add(20);
+        nums.add(30);
+        nums.add(50);
+        nums.add(36);
+        nums.add(60);
+
+        int suma = 0;
+        for (int numero : nums) {
+            suma += numero;
+        }
+        System.out.println("La suma de los nums es: " + suma);
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+        for (int i = 0; i< nums.size(); i++){
+            System.out.println(i + " : " + nums.get(i));
+
+        }
+        ArrayList<String> nombres = new ArrayList<>();
+        nombres.add("valentina");
+        nombres.add("camila");
+        nombres.add("anderson");
+        nombres.add("lucia");
+
+
+        System.out.println("for");
+        for(int i = 0; i < nombres.size(); i++) {
+            System.out.println(nombres.get(i));
+        }
+        {
+            System.out.println("for each:");
+            for(String nombre : nombres);
+            System.out.println(nombres);
+        }
+
+        System.out.println("while:");
+        int i = 0;
+        while (i < nombres.size())
+        {
+            System.out.println(nombres.get(i));
+            i++;
+
         }
     }
 }
